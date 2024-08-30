ef4693d7d14bf0936a8b9694d67c323d4dabe7a4
geneonkeys <geneonkeys@gmail.com>
2024-08-30 16:58:53 -0400

    reallocate all raw index.html changes to .tsx, include hover animation on shark logo for email

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import cassandsharkLogo from './assets/cassandshark.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <h1>S&</h1>
+        <a href="mailto:hello@cassandshark.com" target="_blank">
+          <img src={cassandsharkLogo} className="logo react" alt="Cassandshark logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>SHARK.</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
