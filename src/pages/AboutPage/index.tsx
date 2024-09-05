f4e72dc1575c015816e8ae82d97dad40295821a8
royin345 <royin345@gmail.com>
2024-09-05 14:19:59 -0400

    add the new testimonials from WordPress which is Cassandra Trissler dot com

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -16,11 +16,86 @@ const AboutPage: React.FC = () => {
             <div>🎮 video games </div>
             <div>🎲 board games</div>
             <div>👻 horror</div>
+            <div>🪡 sewing & crafts</div>
             <div>🔴 Pokémon</div>
             <div>📜 writing</div>
             <div>🪵 woodworking</div>
             <div>🚶 touching grass</div>
-            <div>10 photos</div>
+            <div>testimonials</div>
+            <div>
+                Cassandra is not only an incredible UX designer, but she has also been incredible human to have worked with. As her manager, I got a front row
+                seat to how she approaches problems, how she shows empathy for user needs at every step of the UX process, and cares deeply about the outcome
+                and impact of her work. She has tackled several complex products, from energy companies to streaming services, always with a wholistic and
+                thoughtful view of the product and how to put the user first- while still incorporating business goals. She is smart; hard working, and I
+                believe has only had the opportunity to scratch the surface of what she is capable of. I hope that her next opportunity opens all of the doors I
+                wish she had access to here and I’m thankful to have known her and been a small part of her career journey.
+            </div>
+            <div>Kate Ardelan Principal UX Designer at Kin + Carta</div>
+            <div>Horizontal Rule but wavey</div>
+            <div>
+                I’ve had the pleasure of working with Cassandra for the past 7 years and have been consistently impressed by her design abilities, strong
+                teamwork skills, and dedication. Her attention to detail is amazing and her ability to meticulously craft and fine-tune designs sets her apart
+                from others in the industry. Cassandra is also an extremely talented character artist and animator. Her creative talents were invaluable during
+                our time working together at IDSI and her ability to bring ideas to life through her art and animations is truly remarkable. She has a natural
+                ability to create stunning visuals and is always eager to take on new challenges. She is not afraid to step out of her comfort zone with a
+                willingness to adapt and learn quickly. In addition to her design talents, Cassandra is a great communicator and has the ability to work
+                effectively in a team environment or on individual tasks. She is a true team player and is always willing to go above and beyond. Cassandra will
+                make an exceptional addition to any design team and I look forward to the chance to work together again in the future. Cassandra’s expertise in
+                UX/UI design was essential in helping our team deliver high-quality products with a user-centered approach. She was able to work closely with
+                our product owners and stakeholders to understand their needs and translate them into effective design solutions. What impressed me most about
+                Cassandra was their ability to work within the framework of Scrum, always keeping in mind the iterative and incremental nature of our work. She
+                was always willing to adapt and pivot as needed to ensure that our team remained on track and aligned with our goals. Overall, I highly
+                recommend Cassandra for any UX/UI design role. Cassandra is an exceptional team player, a great communicator, and a true asset to any agile
+                team.
+            </div>
+            <div>Ryan O’Donnell Director, Client Lead at Kin + Carta</div>
+            <div>Horizontal Rule but wavey</div>
+            <div>
+                Cassandra is a wonderful designer who regularly surprised me with her range of talents – not only is she a quick learner who’s able to unpack
+                complex business, user and technical requirements, she can also whip out impressive illustrations in seconds, create meaningful educational
+                materials for meaty subjects, and is a champion of any game you throw her way. I hope to have the opportunity to work with Cassandra again, and
+                would recommend her 10/10.
+            </div>
+            <div>Sarah Swartzendruber Director of Product Design at Kin + Carta</div>
+            <div>Horizontal rule but wavey</div>
+            <div>
+                I am writing to highly recommend Cassandra, a skilled UX/UI Designer whom I had the pleasure of working with as a Scrum Master. She was an
+                integral part of our agile team, consistently demonstrating a strong commitment to collaboration, communication, and continuous improvement.
+                Cassandra’s expertise in UX/UI design was essential in helping our team deliver high-quality products with a user-centered approach. She was
+                able to work closely with our product owners and stakeholders to understand their needs and translate them into effective design solutions. What
+                impressed me most about Cassandra was their ability to work within the framework of Scrum, always keeping in mind the iterative and incremental
+                nature of our work. She was always willing to adapt and pivot as needed to ensure that our team remained on track and aligned with our goals.
+                Overall, I highly recommend Cassandra for any UX/UI design role. Cassandra is an exceptional team player, a great communicator, and a true asset
+                to any agile team.
+            </div>
+            <div>Cory Day Scrum Master at Highgate Training Systems</div>
+            <div>Horizontal rule but wavey</div>
+            <div>
+                Cassandra is an exceedingly talented artist and designer who can just as easily realize stylish, modern user interfaces as she can concept out
+                fun and interesting character designs. I worked with Cassandra for two years as a software developer on multiple projects, all of which were
+                vastly improved by her contributions, both in quality and development time. She is a pleasure to work with, constantly practicing her craft, and
+                encourages others to want to improve their own skills. I would recommend Cassandra for any art or design project without hesitation.
+            </div>
+            <div>Brandon Lee Interactive Developer at Intelligent Decision Systems, Inc.</div>
+            <div>Horizontal rule but wavey</div>
+            <div>
+                A multi-talented artist, designer, and illustrator, Cassandra sets a standard of excellence and attention for detail in her work. Over the
+                course of about two years working with Cassandra, I was always amazed by her ability to handle the toughest design requests, effortlessly with
+                such short notice. She has excelled beyond the core elements of design by branching out and adapting her work to fit with developing game
+                engines and mobile applications. With every project she was involved in, from game jams to marketing, she always brought with her a positive
+                energy and a resolve to create the sharpest looking product. Cassandra is a fantastic team player for any position requiring design and art.
+            </div>
+            <div>Austen Seeberg Technical Artist at Highgate Training Systems (Threat Tec)</div>
+            <div>photo</div>
+            <div>photo</div>
+            <div>photo</div>
+            <div>photo</div>
+            <div>photo</div>
+            <div>photo</div>
+            <div>photo</div>
+            <div>photo</div>
+            <div>photo</div>
+            <div>photo</div>
         </>
     )
 }
